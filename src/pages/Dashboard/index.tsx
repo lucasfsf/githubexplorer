02f2d5c06b28d80cd7900eb18a21bed9d5f1f26c
Lucas Fonseca <lucasfreireri@gmail.com>
2020-09-10 17:07:54 -0300

    styled global

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositórios no Github.</Title>;
+};
+
+export default Dashboard;
